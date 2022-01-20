d1e9a6f87003c4bb58c636fc03d08db1d2db174a
Kabilan Sakthivelu <kabilansakthi97@gmail.com>
2022-01-20 18:45:39 +0530

    Added pages and implemented routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import Add from './Components/Add/Add';
+import Edit from './Components/Edit/Edit';
+import View from './Components/View/View';
+import Search from './Components/Search/Search';
+import Error from './Components/Error/Error';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-red-500">Hello World!!</h1>
-    </div>
+    <Router>
+    <Header/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path="/add" element={<Add/>}/>
+        <Route path="/view" element={<View/>}/>
+        <Route path="/edit" element={<Edit/>}/>
+        <Route path="/search" element={<Search/>}/>
+        <Route path="*" element={<Error/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/Search/Search.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Search = () => {
+  return <div><h1>Search</h1></div>;
+};
+
+export default Search;
