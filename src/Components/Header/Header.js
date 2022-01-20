19027f3d3cd04fc45b3b47260ea1ecb37942bb7f
Kabilan Sakthivelu <kabilansakthi97@gmail.com>
2022-01-20 20:20:43 +0530

    Added content for Header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {AiFillHome, AiFillFileAdd, AiOutlineSearch} from 'react-icons/ai';
+import {Link} from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
-  return <div><h1>Header</h1></div>;
+
+  const [isMobileView, setIsMobileView] = useState(false);
+
+  //screenSize function to determine the displayed window size
+
+  const screenSize = () =>{
+    if(window.innerWidth < 768){
+      setIsMobileView(true);
+    }
+    else{
+      setIsMobileView(false);
+    }
+  }
+
+  //useEffect hook to call screenSize function on window resize
+
+  useEffect(()=>{
+    screenSize();
+    window.addEventListener("resize", screenSize);
+    return(()=>{
+    window.removeEventListener("resize", screenSize);  
+    })
+  }, [])
+
+  return (
+  <div className="header">
+
+  {/* App title  */}
+
+  <Link className="title" to="/">Organisation Hierarchy</Link>
+
+  {/* Navbar menu */}
+
+  <div className="navbarIcons">
+  <Link to="/">{isMobileView ? <AiFillHome/> : <p>Home</p>}</Link>
+  <Link to="/add">{isMobileView ? <AiFillFileAdd/> : <p>Add</p>}</Link>
+  <Link to="/search">{isMobileView ? <AiOutlineSearch/> : <p>Search</p>}</Link>
+  </div>
+  </div>
+  );
+
 };
 
 export default Header;
