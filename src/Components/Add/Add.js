6ba3a7fdf7e5f2ea95ee3dd8dc63e4b5a7d9dce9
Kabilan Sakthivelu <kabilansakthi97@gmail.com>
2022-01-24 23:48:34 +0530

    Committing changes made in Add page

--- src/Components/Add/Add.js
+++ src/Components/Add/Add.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {data} from '../../data';
+import './Add.css';
 
 const Add = () => {
-  return <div><h1>Add</h1></div>;
+
+  const [departmentChosen, setDepartmentChosen] = useState();
+
+  const [teamChosen, setTeamChosen] = useState();
+
+  const selectedDept = data.employees[0].children.find((item)=>{
+      return item.id === parseInt(departmentChosen);
+  })
+
+  return (
+  <div>
+  <form >
+  <label htmlFor="department">Department</label>
+  <select id="department" onChange={(e)=>{setDepartmentChosen(e.target.value)}}>
+  <option value="null">Please select</option>
+  <option value="2">HR</option>
+  <option value="3">Engineering</option>
+  <option value="4">Design</option>
+  </select>
+  {departmentChosen &&
+  <div>
+  <label htmlFor="team">Team</label>
+  <select id="team" onChange={(e)=>{setTeamChosen(e.target.value)}}>
+  <option value="null">Please select</option>
+  {selectedDept.children.map((item)=>{
+    return (<option key={item.id} value={item.id}>{item.name}</option>)
+  })}
+  </select>
+  {teamChosen && 
+  <div>
+  <label htmlFor="name"></label>
+  <input type="text" name="name" id="name" />
+  <label htmlFor="email"></label>
+  <input type="email" name="email" id="email" />
+  <label htmlFor="phone"></label>
+  <input type="number" name="phone" id="phone" />
+  </div>
+  }
+  </div>
+  }
+  </form>
+  </div>
+  );
 };
 
 export default Add;
